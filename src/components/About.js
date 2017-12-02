91f43ea5ddc2a58152654c8c916791692c2c1f1a
lucashuet93 <lucashh@microsoft.com>
2017-12-01 21:47:33 -0800

    responsive layout on content

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ class About extends Component {
 	render() {
 		return (
 			<div className="ms-Grid-row">
-				About
+				<div className="ms-Grid-col ms-u-sm1 ms-u-md3"/>
+				<div className="ms-Grid-col ms-u-sm10 ms-u-md6">
+					About
+				</div>
+				<div className="ms-Grid-col ms-u-sm1 ms-u-md3" />
 			</div>
 		);
 	}
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,7 +4,11 @@ class Blog extends Component {
 	render() {
 		return (
 			<div className="ms-Grid-row">
-				Blog
+				<div className="ms-Grid-col ms-u-sm1 ms-u-md3"/>
+				<div className="ms-Grid-col ms-u-sm10 ms-u-md6">
+					Blog
+				</div>
+				<div className="ms-Grid-col ms-u-sm1 ms-u-md3" />
 			</div>
 		);
 	}
