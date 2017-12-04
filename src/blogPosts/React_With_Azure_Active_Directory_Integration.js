991363f56e8134cc07799b6d11c33b9907d79796
lucashuet93 <lucashh@microsoft.com>
2017-12-04 15:57:01 -0800

    write react w/ AD blog postwrite react w/ AD blog post

--- src/blogPosts/React_With_Azure_Active_Directory_Integration.js
+++ src/blogPosts/React_With_Azure_Active_Directory_Integration.js
@@ -68,7 +68,7 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<h3>{greeting}</h3>
+					<span className="ms-font-xxl">{greeting}</span>
 				</header>
 				<button onClick={this.state.azureADHelper.login}>Login</button>
 				<button onClick={this.state.azureADHelper.logout}>Logout</button>
@@ -91,14 +91,16 @@ componentWillMount() {
 	}
 }`;
 		return (
-			<div className="ms-font-m blogPost">
-				<h1>Integrate Azure Active Directory into React.js using ADAL.js</h1>
+			<div className="ms-font-m ms-fontWeight-semilight blogPost">
+				<div className="blogPostTitle">
+					<span className="ms-font-su">Integrate Azure Active Directory into React.js using ADAL.js</span>
+				</div>
 
 				<p>Adal.js allows developers to utilize Azure Active Directory for authentication in single-page apps, but the docs state the "library is optimized for working together with AngularJS".
 					Does that mean you can't use it in other SPA frameworks? Of course not!
 					Let's create a class that can be utilized in a variety of SPA frameworks, with a specific implementation into React.js. </p>
 
-				<h3>Create Your React Project</h3>
+				<span className="ms-font-xxl">Create Your React Project</span>
 				<p>If you haven't installed create-react-app globally before, do so now:</p>
 				<SyntaxHighlighter language='javascript' style={monokaiSublime}>{codeString1}</SyntaxHighlighter>
 				<p>Create-react-app is a library that provisions a boilerplate react app for you without build configuration, complete with hot reloading. To create a new React project called "react-ad", run:</p>
@@ -106,7 +108,7 @@ componentWillMount() {
 				<p>Once create-react-app is finished provisioning your project, open the folder and run it:</p>
 				<SyntaxHighlighter language='javascript' style={monokaiSublime}>{codeString3}</SyntaxHighlighter>
 
-				<h3>Create an Active Directory v2 App</h3>
+				<span className="ms-font-xxl">Create an Active Directory v2 App</span>
 				<p>You'll need to spin up a new Active Directory v2 application you can leverage in your SPA.
 					Navigate to the new <a href="https://apps.dev.microsoft.com">Microsoft Application Registration Portal</a>, find the <span className="ms-fontWeight-semibold">Converged Applications</span> section, and click <span className="ms-fontWeight-semibold">Add an app</span>.</p>
 				<p className="italics">Note: You could also use "Azure AD only applications"</p>
@@ -121,7 +123,7 @@ componentWillMount() {
 				<img src={require("../images/PlatformsWithRedirect.jpg")}></img>
 				<p>One last thing before you're all set. Navigate to the <span className="ms-fontWeight-semibold">Properties</span> section at the top of the page, and copy down your <span className="ms-fontWeight-semibold">Application Id</span>.</p>
 
-				<h3>Create a Reusable AzureADHelper class</h3>
+				<span className="ms-font-xxl">Create a Reusable AzureADHelper class</span>
 				<p>In your React project, you'll create a helper class with 3 methods that login, logout, and read the current user information. The AADv2 Application you just created will handle these requests.</p>
 				<p>Go ahead and install the adal.js package into your project:</p>
 				<SyntaxHighlighter language='javascript' style={monokaiSublime}>{codeString4}</SyntaxHighlighter>
@@ -131,7 +133,7 @@ componentWillMount() {
 				<p>The bind method allows you to use the class methods in any component without confusing the meaning of <span className="ms-fontWeight-semibold">this</span>. You may not require the bind calls in other SPA frameworks.</p>
 				<p>You can now instantiate the azureADHelper class in any React.js component (or other SPA framework) and run authentication methods! Pretty simple, right?</p>
 
-				<h3>Use the AzureADHelper class in the React Project</h3>
+				<span className="ms-font-xxl">Use the AzureADHelper class in the React Project</span>
 				<p>Navigate to app.js and utilize your helper class:</p>
 				<SyntaxHighlighter language='javascript' style={monokaiSublime}>{codeString6}</SyntaxHighlighter>
 				<p>A small bit of boilerplate has been removed and replaced with 3 buttons. An instance of the AzureADHelper class is instantiated in the constructor, and the class is used by the buttons to login, logout, and read the current user at anytime (your name will display if you have logged in). Run the project and try it out! </p>
