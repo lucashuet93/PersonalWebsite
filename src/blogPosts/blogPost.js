6bf1062dc7e8450cad6d23a3fca1d0c08eb887b8
lucashuet93 <lucashh@microsoft.com>
2017-12-04 23:31:49 -0800

    blog post homepage first run

--- src/blogPosts/blogPost.js
+++ src/blogPosts/blogPost.js
@@ -1,7 +1,8 @@
 export class BlogPost {
-	constructor(url, title, content){
+	constructor(url, title, content, tags){
 		this.url = url;
 		this.title = title;
 		this.content = content;
+		this.tags = tags
 	}
 }
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 import { BlogPost, Running_Local_Bots_In_Microsoft_Teams, React_With_Azure_Active_Directory_Integration } from '../blogPosts';
 import BlogFooter from './BlogFooter';
+import { List, Icon, FocusZone, FocusZoneDirection } from 'office-ui-fabric-react';
 
 class Blog extends Component {
 	constructor(p) {
 		super()
+	}
+	renderListItem(blog) {
+		let endpoint = `/blog/${blog.url}`
+		return (
+			<div className="ms-Grid-row blogListItem" data-is-focusable={true}>
+				<div className="ms-Grid-col ms-u-sm5 ms-u-md5">
+					<NavLink className="link-text" to={endpoint}><span className="ms-font-l ms-font-themePrimary link-text">{blog.title}</span></NavLink>
+				</div>
+				<div className="ms-Grid-col ms-u-sm6 ms-u-md6">
+					{blog.tags.map((tag) => {
+						return (
+							<div className='tag'>
+								<span className="ms-font-m tagText ms-fontWeight-regular">{tag}</span>
+							</div>
+						)
+					})}
+				</div>
+				<div className="ms-Grid-col ms-u-sm1 ms-u-md1"><i class="ms-Icon ms-Icon--ChevronRight" aria-hidden="true"></i></div>
+			</div>
+		)
 	}
 	generateNotFoundPage() {
 		return (
@@ -25,13 +46,22 @@ class Blog extends Component {
 			<div>
 				<div className="ms-Grid-row">
 					<div className="ms-Grid-col ms-u-sm1 ms-u-md2" />
-					<div className="ms-Grid-col ms-u-sm10 ms-u-md8">
-						<ul>
-							{blogs.map((blog) => {
-								let endpoint = `/blog/${blog.url}`
-								return <li><NavLink className="link-text" to={endpoint}><span className="ms-font-xl link-text">{blog.title}</span></NavLink></li>
-							})}
-						</ul>
+					<div className="ms-Grid-col ms-u-sm10 ms-u-md8 blogList">
+						<div className="ms-Grid-row blogListItemHeader">
+							<div className="ms-Grid-col ms-u-sm5 ms-u-md5">
+								<span className="ms-font-xl ms-font-themePrimary link-text">Title</span>
+							</div>
+							<div className="ms-Grid-col ms-u-sm5 ms-u-md5">
+								<span className="ms-font-xl ms-font-themePrimary link-text">Tags</span>
+							</div>
+							<div className="ms-Grid-col ms-u-sm1 ms-u-md1" />
+						</div>
+						<FocusZone direction={FocusZoneDirection.vertical}>
+							<List
+								items={blogs}
+								onRenderCell={this.renderListItem.bind(this)}
+							/>
+						</FocusZone>
 					</div>
 					<div className="ms-Grid-col ms-u-sm1 ms-u-md2" />
 				</div>
@@ -39,8 +69,9 @@ class Blog extends Component {
 		);
 	}
 	createBlogs() {
-		let localBotsInTeams = new BlogPost("running-local-bots-in-microsoft-teams", "Running Local Bots in Microsoft Teams", <Running_Local_Bots_In_Microsoft_Teams />);
-		let reactWithActiveDirectory = new BlogPost("react-with-azure-active-directory-integration", "React With Azure Active Directory Integration", <React_With_Azure_Active_Directory_Integration />);
+
+		let localBotsInTeams = new BlogPost("running-local-bots-in-microsoft-teams", "Running Local Bots in Microsoft Teams", <Running_Local_Bots_In_Microsoft_Teams />, ["Microsoft Teams", "Bots"]);
+		let reactWithActiveDirectory = new BlogPost("react-with-azure-active-directory-integration", "React With Azure Active Directory Integration", <React_With_Azure_Active_Directory_Integration />, ["React.js", "Active Directory"]);
 		let blogPosts = [localBotsInTeams, reactWithActiveDirectory];
 		return blogPosts;
 	}
