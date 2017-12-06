b020d5e459f3bd1e6757cd4315c997d9df3c74ac
lucashuet93 <lucashh@microsoft.com>
2017-12-05 16:42:49 -0800

    create logo for display on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,16 @@ class Header extends React.Component {
 	render() {
 		return (
 			<div className="ms-Grid-row ms-bgColor-black ms-fontColor-white header">
-				<div className="ms-Grid-col ms-u-sm6 ms-u-md4 ms-font-xxl">LH2</div>
-				<div className="ms-Grid-col ms-u-hiddenSm ms-u-md5" />
+				<div className="ms-Grid-col ms-u-sm6 ms-u-md4 ms-font-xxl">
+					<div style={{verticalAlign:"top", display:"inline-block"}}><span className="letter">L</span></div>
+					<div style={{verticalAlign:"top", display:"inline-block"}}><span className="letter">H</span></div>
+					<div style={{verticalAlign:"top", display:"inline-block"}}><span className="letter">H</span></div>
+				</div>
+				<div className="ms-Grid-col ms-u-hiddenSm ms-u-md6" />
 				<div className="ms-Grid-col ms-u-sm3 ms-u-md1 headerDiv">
 					<NavLink className="link-text" to="/blog"><span className="ms-font-xl ms-fontColor-white link-text">Blog</span></NavLink></div>
 				<div className="ms-Grid-col ms-u-sm3 ms-u-md1 headerDiv">
 					<NavLink className="link-text" to="/about"><span className="ms-font-xl ms-fontColor-white link-text">About</span></NavLink></div>
-				<div className="ms-Grid-col ms-u-hiddenSm ms-u-md1" />
 			</div>
 		);
 	}
