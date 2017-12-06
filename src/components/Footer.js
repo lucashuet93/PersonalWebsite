9d4297ac3d9d2f0cfabf75afe494866a28416505
lucashuet93 <lucashh@microsoft.com>
2017-12-05 16:37:39 -0800

    footer icon spacing, perfectly centered

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = (props) => {
 			<div className="ms-Grid-col ms-u-sm2 ms-u-md2">
 			</div>
 			<div className="ms-Grid-col ms-u-sm8 ms-u-md8 iconDiv">
-				<a className="icon" href="https://github.com/lucashuet93"><img className="icon" src={require('../images/githubBlack.png')} /></a>
-				<a className="icon"  href="https://twitter.com/lucashuet93"><img className="icon" src={require('../images/twitterBlack.png')} /></a>
-				<a className="icon"  href="https://www.linkedin.com/in/lucas-huet-hudson-0a7110a9/"><img className="icon linkedin" src={require('../images/linkedinBlack.png')} /></a>
+				<a href="https://github.com/lucashuet93"><img className="icon" src={require('../images/githubBlack.png')} /></a>
+				<a href="https://twitter.com/lucashuet93"><img className="icon" src={require('../images/twitterBlack.png')} /></a>
+				<a href="https://www.linkedin.com/in/lucas-huet-hudson-0a7110a9/"><img className="icon linkedin" src={require('../images/linkedinBlack.png')} /></a>
 			</div>
 			<div className="ms-Grid-col ms-u-sm2 ms-u-md2">
 			</div>
